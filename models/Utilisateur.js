372a4d2e1d9954730f05ccfc5ad55bc9c78b67c6
dulmaranspace4 <serulmrls@hotmail.com>
2025-08-31 07:57:58 -0700

    feat: initial commit for models/Utilisateur.js

---
+++ models/Utilisateur.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const utilisateurSchema = new mongoose.Schema({
+  nom: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  motDePasse: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Utilisateur', utilisateurSchema);
